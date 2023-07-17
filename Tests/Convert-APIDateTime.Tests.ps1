e2bfba0e0730b41b98aec4607e752ae935ccd8f0
Tony Pagliaro <tony@pagliaro.co>
2023-07-17 14:03:56 -0400

    Update test to ensure date is UTC kind. Add test for newly supported string format.

--- Tests/Convert-APIDateTime.Tests.ps1
+++ Tests/Convert-APIDateTime.Tests.ps1
@@ -11,6 +11,7 @@ Describe -Name 'Private/Convert-APIDateTime' -Tag 'Private', 'Convert-APIDateTim
         $cases = 'Mon Jan 10 17:12:14 UTC 2019',
         'Mon Mar 11 09:12:14 UTC 2017',
         'Mon Sep 12 23:12:14 UTC 2018',
+        '2023-07-17T09:59:58.453Z',
         'Mon Dec 13 14:12:14 UTC 2006' | ForEach-Object {
             @{'DateTimeString' = $_}
             
@@ -18,22 +19,23 @@ Describe -Name 'Private/Convert-APIDateTime' -Tag 'Private', 'Convert-APIDateTim
 
         It -Name "Get-RubrikAPIData - <DateTimeString> test" -TestCases $cases {
             param($DateTimeString)
-            Convert-APIDateTime -DateTimeString $DateTimeString.ToString() |
-                Should -BeOfType DateTime
+            $Date = Convert-APIDateTime -DateTimeString $DateTimeString.ToString()
+            $Date | Should -BeOfType DateTime
+            $Date.Kind | Should -Be 'Utc'
         }
     }
     
     Context -Name 'Error handling' -Fixture {
         It -Name 'February 30 - Should not have output' -Test {
-            Convert-APIDateTime -DateTimeString 'Mon Feb 30 09:12:14 UTC 2017' |
+            Convert-APIDateTime -DateTimeString 'Mon Feb 30 09:12:14 UTC 2017' -ea 0 |
                 Should -BeExactly $null
         }
         It -Name 'Movember - Should not have output' -Test {
-            Convert-APIDateTime -DateTimeString 'Mon Mov 30 09:12:14 UTC 2018' |
+            Convert-APIDateTime -DateTimeString 'Mon Mov 30 09:12:14 UTC 2018' -ea 0 |
                 Should -BeExactly $null
         }
         It -Name '25th hour - Should not have output' -Test {
-            Convert-APIDateTime -DateTimeString 'Mon Sep 30 25:12:14 UTC 2018' |
+            Convert-APIDateTime -DateTimeString 'Mon Sep 30 25:12:14 UTC 2018' -ea 0 |
                 Should -BeExactly $null
         }
     }
